90e1c36b36891b00333aef30e82748b238f148d3
Jehaz <931535840@qq.com>
2019-02-16 09:27:49 +0000

    shouye

--- page/loading/loading.js
+++ page/loading/loading.js
@@ -1,6 +1,7 @@
 Page({
   data:{
-    picture:"/images/1.jpg"
+    picture:"/images/1.jpg",
+    logo:"/images/logo.png",
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
@@ -18,5 +19,20 @@ Page({
   //   })
   //  },1000)
      
+  },
+  //获取微信手机号码
+  getPhoneNumber: function (e) {
+    console.log(e.detail.errMsg)
+    if (e.detail.errMsg == 'getPhoneNumber:ok') {
+      wx.switchTab({
+        url: '/page/index/index',
+      })
+    }
+    else{
+      wx.showToast({
+        title: '请先登录！',
+      })
+    }
+    
   },
 })
